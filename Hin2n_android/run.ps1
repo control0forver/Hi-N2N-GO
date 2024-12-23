20a12280d422ef8f8cc7f3ac7c93bf2f63f74d4b
LGF-OnLaptop <qwe3239215285@163.com>
2024-12-24 02:12:39 +0800

    Update run.ps1 and build.gradle

--- Hin2n_android/run.ps1
+++ Hin2n_android/run.ps1
@@ -1,29 +1,63 @@
-$jsonFilePath = "app/build/outputs/apk/normalAllarch/debug/output.json"
+$rootDir = "app"
 
-while (1) {
+$jsonFiles = Get-ChildItem -Path $rootDir -Recurse -Filter "output.json" -File | ForEach-Object { $_.FullName }
+
+if ($jsonFiles.Length -eq 0)
+{
+    Write-Host 'No any "output.json" found.'
+}
+
+for ($i = 0; $i -lt $jsonFiles.Length; $i++) {
+    Write-Host "$( [char]9679 ) $( $i + 1 ). $( $jsonFiles[$i] )"
+}
+
+while (1)
+{
+    Write-Host ("Enter the index of json file(1-{0}):" -f $jsonFiles.Count)
+    $userInput = Read-Host
+
+    if (($userInput -lt 1) -or ($userInput -gt $jsonFiles.Count))
+    {
+        Write-Host "Invalid input, please try again!"
+    }
+    else
+    {
+        break
+    }
+}
+
+$jsonFilePath = $jsonFiles[$userInput - 1]
+
+while (1)
+{
     Write-Host "Press Any Key to Start..."
     [void][System.Console]::ReadKey($true)
     & clear
 
-    try {
+    try
+    {
         $jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
 
         $apkFileName = $jsonContent.elements[0].outputFile
         $apkFilePath = Join-Path -Path (Split-Path -Path $jsonFilePath) -ChildPath $apkFileName
 
-        if (!(Test-Path -Path $apkFilePath -PathType Leaf)) {
+        if (!(Test-Path -Path $apkFilePath -PathType Leaf))
+        {
             Write-Error "APK file dose not exists: $apkFilePath"
             break
         }
 
         & adb install "$apkFilePath"
-        if ($LASTEXITCODE -ne 0) {
+        if ($LASTEXITCODE -ne 0)
+        {
             Write-Error "ADB Install Failed with Code: $LASTEXITCODE"
             break
         }
 
         & adb shell am start -n wang.switchy.hin2n/.activity.MainActivity
-    } catch {
+    }
+    catch
+    {
         Write-Error "Error: $_"
         break
     }
